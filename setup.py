b22109533bb7046c187fed1b5dc1420804ef05a3
Malik Talha <maliktalha1515@gmail.com>
2025-09-11 20:50:29 +0500

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Malik Talha",
+    author_email="maliktalha1515@gmail.com",
+    packages=find_packages()
+)
